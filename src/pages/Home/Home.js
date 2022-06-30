4c808d3bfac62d3d84dba9abe1505146a360a7c9
unknown <atiqit52@gmail.com>
2022-06-30 21:23:46 +0600

    task delete succesfully

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTask from '../AddTask/AddTask';
+import { toast } from 'react-toastify';
+import Tasks from '../Task/Tasks';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
+    const [tasks, setTasks] = useState([]);
 
+    useEffect( () => {
+        fetch('http://localhost:8000/task')
+        .then(res => res.json())
+        .then(data => setTasks(data));
+    }, [])
 
+  const handleTaskDelete = id =>{
+    const proceed =window.confirm ('Are you sure you want to delete?');
+    if(proceed){
+        console.log('deleting task with id', id);
+        const url = `http://localhost:8000/task/${id}`;
+        fetch(url, {
+            method: 'DELETE'
+        })    
+        .then(res =>res.json())
+        .then(data => {
+           if(data.deletedCount > 0){
+            console.log('deleted')
+            const remaining = tasks.filter(user => user._id !== id);
+            setTasks(remaining)
+            toast(' Task Deleted successfully ')
+           }
+        })
+    }
+  }
 
     return (
         <div >
-            {/* <input type="text" placeholder="Type Task" class="input input-bordered input-sm w-full max-w-xs" /> */}
-            {/* <button class="btn btn-sm px-5 h-2 ml-2">Add Task</button> */}
             <AddTask></AddTask>
+          <div className='mb-20 '>
+            {
+                tasks.map(task => 
+                   
+                  <div className='grid justify-center  '>
+                    <div className='grid grid-cols-4 place-content-center w-[800px] border p-2   '>
+                     <FontAwesomeIcon className='bg-yellow-500 text-white text-2xl rounded-full' icon={faCheck}> </FontAwesomeIcon>
+                    <p className=''>{task.name}</p>
+                    <p className='bg-green-500 rounded-lg text-white h-7 w-20 text-center'>update</p>
+                    <button onClick={() => handleTaskDelete(task._id)} className='bg-red-500 rounded-lg text-white h-7 w-20 text-center'>Delete</button>
+                    {/* <button onClick={() => handleUserDelete(user._id)} > X </button> */}
+
+                 </div>
+                    </div>
+
+                )
+            }
+          </div>
         </div>
     );
 };
---
+++ src/pages/Task/Task.js
@@ -0,0 +1,43 @@
+// import React, { useEffect, useState } from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCheck, faCoffee } from '@fortawesome/free-solid-svg-icons';
+// import { toast } from 'react-toastify';
+// const Task = ({task}) => {
+//     console.log(task.name)
+//     const [tasks, setTasks] = useState([]);
+
+//     useEffect( () => {
+//         fetch('http://localhost:8000/task')
+//         .then(res => res.json())
+//         .then(data => setTasks(data));
+//     }, [])
+
+//     const handleUserDelete = id =>{
+//         const proceed =window.confirm ('Are you sure you want to delete?');
+//         if(proceed){
+//             console.log('deleting task with id', id);
+//             const url = `http://localhost:5000/task/${id}`;
+//             fetch(url, {
+//                 method: 'DELETE'
+//             })    
+//             .then(res =>res.json())
+//             .then(data => {
+//                if(data.deletedCount > 0){
+//                 console.log('deleted')
+//                 const remaining = tasks.filter(user => user._id !== id);
+//                 setTasks(remaining)
+//                 toast(' Task Deleted successfully ')
+//                }
+//             })
+//         }
+//       }
+    
+//     return (
+//         <div>
+//             {task.name}
+//             <FontAwesomeIcon icon={faCheck}> </FontAwesomeIcon>
+//         </div>
+//     );
+// };
+
+// export default Task;
